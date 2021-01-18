b7531984dd0d8a1be377cd5bd4007839b4b2acb2
darogel <darogel96@gmail.com>
2021-01-18 11:48:01 -0800

    Creacion inicial del Trabajo de Titulacion

---
+++ plan_mejoras/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Plan Mejoras",
+    "description":"Modulo de software para el Plan Mejoras de la Evaluacion al Desempeno Docente para la CIS/C",
+    "author": "Darwin Rogel - Robin Cordova",
+
+    "summary": """
+        Este es un trabajo de titulacion para la obtencion del titulo de Ingeniero en Sistemas
+        Permite la optimizacion tecnica del Plan Mejoras de la EDD. en la CIS/C de la Universidad Nacional de Loja
+        """,
+
+    "website": "https://unl.edu.ec/",
+
+    "category": "Seguimiento",
+
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+
+    ]
+}
