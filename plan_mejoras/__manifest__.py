13dc20bbb7d0ab1150288e8e3b97b252647f8799
darogel <darogel96@gmail.com>
2021-01-25 16:08:24 -0800

    Creacion de Vista Docente, Roles y permisos. Co-authored-by: RobinLeninU <robin.cordova@unl.edu.ec>

--- plan_mejoras/__manifest__.py
+++ plan_mejoras/__manifest__.py
@@ -19,8 +19,6 @@
         "security/res_groups.xml",
         "security/ir_rule.xml",
         "security/ir_model_access.xml",
-        "report/template.xml",
-        "report/report.xml",
         "views/views.xml"
     ]
 }
